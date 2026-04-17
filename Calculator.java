e268c49de63af15e64cc3913973ad48ad988a10a
unknown <1705arunkumar@gmail.com>
2026-04-17 15:36:10 +0530

    Calculator class is created

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+class Calculator{
+
+    static int add(int a, int b){
+        return a+b;
+    }
+
+    static int sub(int a, int b){
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("Addition: "+add(10,20));
+        System.out.println("Subraction: "+ sub(10,30));
+    }
+}
