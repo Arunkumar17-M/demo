87e9299d942334e34803523dcdda5d7b47e5daf9
unknown <1705arunkumar@gmail.com>
2026-04-18 19:17:58 +0530

    added Division method

--- Calculator.java
+++ Calculator.java
@@ -12,10 +12,15 @@ class Calculator{
         return a*b;
     }
 
+    static int div(int a, int b){
+        return a/b;
+    }
+
     public static void main(String[] args) {
         
         System.out.println("Addition: "+add(10,20));
         System.out.println("Subraction: "+ sub(10,30));
         System.out.println("multiplication: "+mul(10, 30));
+        System.out.println("Division: "+div(10, 5));
     }
 }
