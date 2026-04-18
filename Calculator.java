e50d9784928dcf734a612bb774b61ac6face61d8
unknown <1705arunkumar@gmail.com>
2026-04-18 19:07:14 +0530

    uncommented multiplication

--- Calculator.java
+++ Calculator.java
@@ -8,14 +8,14 @@ class Calculator{
         return a-b;
     }
 
-    // static int mul(int a, int b){
-    //     return a*b;
-    // }
+    static int mul(int a, int b){
+        return a*b;
+    }
 
     public static void main(String[] args) {
         
         System.out.println("Addition: "+add(10,20));
         System.out.println("Subraction: "+ sub(10,30));
-        // System.out.println("multiplication: +"+mul(10, 30));
+        System.out.println("multiplication: +"+mul(10, 30));
     }
 }
